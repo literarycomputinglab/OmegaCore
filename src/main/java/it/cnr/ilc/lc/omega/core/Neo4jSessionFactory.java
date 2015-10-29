19c97070ca20fdb4e39a1272a96dea03e6310259
angelo <angelo@dylan2>
2015-10-29 14:03:16 +0100

    session factory map

--- src/main/java/it/cnr/ilc/lc/omega/core/Neo4jSessionFactory.java
+++ src/main/java/it/cnr/ilc/lc/omega/core/Neo4jSessionFactory.java
@@ -1,5 +1,10 @@
 package it.cnr.ilc.lc.omega.core;
 
+import java.util.Collections;
+import java.util.Hashtable;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentMap;
 import org.neo4j.ogm.session.Session;
 import org.neo4j.ogm.session.SessionFactory;
 
@@ -11,6 +16,12 @@ public class Neo4jSessionFactory {
 
     private static final SessionFactory SESSION_FACTORY = new SessionFactory("it.cnr.ilc.lc.omega");
     private static final Session Neo4jSession = SESSION_FACTORY.openSession("http://localhost:7474"); //FIXME Messo singleton per evitare nullpointer dovuto alla rigenerazione della session
+    // pool di sessioni per gestire le risorse
+    // gestire una mappa di sessioni per currentThread
+
+//    private static final Map<Thread,Session> sessionMap =  new Hashtable<>(); // obsolete
+    private static final ConcurrentMap<Thread, Session> sessionMap = new ConcurrentHashMap<>();
+
     static {
         System.setProperty("username", "neo4j");
         System.setProperty("password", "omega");
@@ -18,8 +29,20 @@ public class Neo4jSessionFactory {
 
     public static Session getNeo4jSession() {
 //        return SESSION_FACTORY.openSession("http://wafi.iit.cnr.it:8074");
-       // return SESSION_FACTORY.openSession("http://localhost:7474"); 
-        return Neo4jSession;
+        // return SESSION_FACTORY.openSession("http://localhost:7474"); 
+        Thread currentThread = Thread.currentThread();
+        Session session = null;
+
+//        if(sessionMap.containsKey(currentThread)){
+//            Session session = sessionMap.get(currentThread);
+        session = (sessionMap.containsKey(currentThread)) ?  sessionMap.get(currentThread) : createSession(currentThread);
+        return session; // FIXME, vedasi sopra.
     }
 
+    private static synchronized Session createSession(Thread currentThread){
+        Session session = SESSION_FACTORY.openSession("http://localhost:7474");
+        sessionMap.put(currentThread, session);
+        return session;
+    }
+    
 }
